0c752a192b910dc86a55e6ca9fcfbc71972cbca4
Quentin Lamare <91269485+qlaCode@users.noreply.github.com>
2025-01-12 23:50:14 +0100

    build fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["@sanity/image-url"],
+    },
+  },
+  optimizeDeps: {
+    include: ["@sanity/image-url"],
+  },
+});
