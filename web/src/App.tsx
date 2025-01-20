cdd487c11a73c76e2b93d6c132768349e7c126d6
Quentin Lamare <91269485+qlaCode@users.noreply.github.com>
2025-01-20 17:39:00 +0100

    Logo placement

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,15 +6,15 @@ import BlogCardList from "./components/BlogCardList";
 function App() {
   return (
     <main className="flex flex-col items-center justify-center gap-6 m-6">
-      <Link to="/" className="relative">
-        <div className="flex flex-row items-center relative">
-          <img
-            src="./src/assets/woodpecker.gif"
-            alt="Woodpecker"
-            className="w-16 absolute right-full top-1/2 -translate-y-1/2"
-          />
-          <h1 className="text-[#10A588] text-5xl font-mono">Woodform</h1>
-        </div>
+      <img
+        src="./src/assets/woodpecker.gif"
+        alt="Woodpecker"
+        className="fixed top-4 right-0 w-16"
+      />
+      <Link to="/">
+        <h1 className="text-[#10A588] text-4xl font-mono font-medium">
+          Woodform
+        </h1>
       </Link>
       <h3 className="text-slate-800 text-xl font-mono">Quentin Lamare</h3>
       <Routes>
